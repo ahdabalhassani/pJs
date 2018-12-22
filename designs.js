bf673d412548a900aaeeb7e10269a580a7441cd1
ahdabalhassani <ahdabalhassani@gmail.com>
2018-12-23 01:58:54 +0300

    new changes for project

--- designs.js
+++ designs.js
@@ -1,15 +1,20 @@
+//  Here we select color input
 const gColor = document.getElementById("colorPicker");
+//  Here we select size input
 const gHeight = document.getElementById('inputHeight');
 const gWidth = document.getElementById('inputWidth');
 const gCanvas = document.getElementById('pixelCanvas');
 
+// Here When we clicked the box, color it in
 function clickedBox(event) {
   const color = gColor.value;
   event.target.style.backgroundColor = color;
 }
 
+// Here When we the user submitted the size, call makeGrid()
 function makeGrid() {
   gCanvas.innerHTML = '';
+// Here we Create fragment to build (the rows and columns) - to make table on
   const fragment = document.createDocumentFragment();
 
   for (let r = 0; r < gHeight.value; r++) {
@@ -21,6 +26,7 @@ function makeGrid() {
     tr.addEventListener('click', clickedBox);
     fragment.appendChild(tr);
   }
+  // Here we Push fragment onto DOM
   gCanvas.appendChild(fragment);
 };
 
